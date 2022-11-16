1ebb8b2ecd9d8fcecf01e1f6a626f666123e5e79
Robin Kocken <robin.a.kocken@gmail.com>
2022-11-16 13:38:58 +0100

    Movement and Camera

---
+++ Assets/Robin/Scripts/AnimateOnInput.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateOnInput : MonoBehaviour
+{
+    public Animator handAnimation;
+
+    public InputActionProperty pinchAnimation;
+    public InputActionProperty gripAnimation;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float triggerValue = pinchAnimation.action.ReadValue<float>();
+        handAnimation.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimation.action.ReadValue<float>();
+        handAnimation.SetFloat("Grip", gripValue);
+    }
+}
